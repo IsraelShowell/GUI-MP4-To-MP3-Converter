adc0ed9ab74ab9dc1e33c0ae12530baf70bf22db
IsraelShowell <135654623+IsraelShowell@users.noreply.github.com>
2023-11-02 13:39:31 -0400

    Uploaded GUI_MP4_To_MP3_Converter.py
    I used code from my previous GUI Python projects to make the GUI again. Currently, the file is processed rather slow, so I will look for ways to optimizing the process.

---
+++ GUI_MP4_To_MP3_Converter.py
@@ -0,0 +1,46 @@
+#Creator: Israel Showell
+#Date Created: 11-2-23
+#Allows a user to convert an mp4 file to an mp3 file by entering a path to the directory where the file is located.
+#The mp3 file will be generated in the directory where the Python script is.
+
+# Imports the tkinter library for GUI
+import tkinter as tk
+from tkinter import messagebox
+
+#Imports the moviepy.editor that controls the conversion
+from moviepy.editor import *
+
+
+def convertMP4():
+    
+    Conv = Conv_entry.get() #Gets the text stored in the text area
+    
+    print(Conv) #Prints the entered path.
+
+    # Loads the mp4 file
+    video = VideoFileClip(Conv)
+
+    # Extracts audio from video
+    video.audio.write_audiofile(Conv + ".mp3")
+
+
+
+# Creates the main window
+root = tk.Tk()
+root.title("GUI MP4 to MP3 Converter")  # Sets the window title
+
+# Creates input fields and labels
+Conv_label = tk.Label(root, text="Enter the path to your mp4 file:")  # Create a label
+Conv_label.pack()  # Packs the label into the window
+Conv_entry = tk.Entry(root)  # Creates an input field to enter the path to the file
+Conv_entry.pack()  # Packs the input field into the window
+
+
+run_button = tk.Button(root, text="Convert MP4", command=convertMP4)  # Creates a button that calls the function
+run_button.pack()  # Packs the button into the window
+
+root.mainloop()
+
+
+
+
